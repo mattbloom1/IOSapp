954001d5ebac9ed703c5382fc4628301c9ac77ad
Matt Bloomfield <matthewb3333@gmail.com>
2024-11-12 17:21:17 -0700

    Initial Commit

---
+++ EaseNotes/EaseNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaseNotesApp.swift
+//  EaseNotes
+//
+//  Created by David Ford on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EaseNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
