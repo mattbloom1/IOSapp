11c8aeb9c9a40f1aa53abad55550ffc8f8d2cf08
Matt Bloomfield <matthewb3333@gmail.com>
2024-11-25 23:49:51 -0500

    initial

--- EaseNotes/EaseNotesApp.swift
+++ EaseNotes/EaseNotesApp.swift
@@ -1,17 +1,22 @@
-//
-//  EaseNotesApp.swift
-//  EaseNotes
-//
-//  Created by David Ford on 11/12/24.
-//
-
 import SwiftUI
+import SwiftData
 
 @main
 struct EaseNotesApp: App {
+    let modelContainer: ModelContainer
+
+    init() {
+        do {
+            modelContainer = try ModelContainer(for: Project.self)
+        } catch {
+            fatalError("Could not initialize ModelContainer")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(modelContainer)
     }
 }
